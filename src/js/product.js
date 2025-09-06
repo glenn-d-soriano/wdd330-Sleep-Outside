4945f7ef000a2196a6315c19a0ebd9e2d41b8380
glenn-d-soriano <glenndannielsoriano@gmail.com>
2025-09-05 22:47:26 -0500

    Update

--- src/js/product.js
+++ src/js/product.js
@@ -1,11 +1,29 @@
-import { setLocalStorage } from "./utils.mjs";
+import { setLocalStorage, getLocalStorage } from "./utils.mjs";
 import ProductData from "./ProductData.mjs";
 
 const dataSource = new ProductData("tents");
 
+// Function to add a product to the cart
 function addProductToCart(product) {
-  setLocalStorage("so-cart", product);
+  // get current cart or empty array
+  let cart = getLocalStorage("so-cart") || [];
+
+  // Find the existing item in the cart by its ID
+  const existingItem = cart.find(item => item.Id === product.Id);
+
+  if (existingItem) {
+    // If the item exists, increment its quantity
+    existingItem.Quantity = (existingItem.Quantity || 1) + 1;
+  } else {
+    // If the item doesn't exist, add it to the cart with a quantity of 1
+    product.Quantity = 1;
+    cart.push(product);
+  }
+
+  // save back to localStorage
+  setLocalStorage("so-cart", cart);
 }
+
 // add to cart button event handler
 async function addToCartHandler(e) {
   const product = await dataSource.findProductById(e.target.dataset.id);

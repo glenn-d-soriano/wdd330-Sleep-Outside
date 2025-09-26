31bde3c77090e42a39024af9b956a706e368315a
migarcos <migark@hotmail.com>
2025-09-25 19:23:47 -0500

    form validation corrected

--- src/js/checkout.js
+++ src/js/checkout.js
@@ -7,14 +7,15 @@ const checkOP = new CheckoutProcess("so-cart", "checkout-summary");
 
 checkOP.init();
 
-
-
 // Add event listeners to fire calculateOrderTotal when the user changes the zip code
 document.querySelector("#zip").addEventListener("blur", checkOP.calculateOrderTotal.bind(checkOP));
 
 // listening for click on the button
 document.querySelector("#checkoutSubmit").addEventListener("click", (e) => {
-  e.preventDefault();
-
-  checkOP.checkout();
+  if (!form.checkValidity()) {
+      e.preventDefault(); // Stop form submission
+      alert('Please fill out all required fields correctly.');
+    } else {
+      checkOP.checkout();
+    }  
 });
